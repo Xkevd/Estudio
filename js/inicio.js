82bf06dcb1f06d37d7d34235c16590a95786c000
xkevd <kevinalex11@hotmail.com>
2024-02-08 10:54:56 -0300

    completo menos contacto

--- js/inicio.js
+++ js/inicio.js
@@ -2,16 +2,20 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80){
-        document.querySelector("#header-social").style.display = "none";
+    if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40){
         document.querySelector("#logoandtitle img").style.height = "4rem";
         document.querySelector("#logoandtitle h2").style.fontSize = "1rem";
+        document.querySelector("#logoandtitle h1").style.fontSize = "0.5rem";
+        document.querySelector("#header").style.height = "fit-content";
+        document.querySelector("#header").style.padding = "0.5rem";
 
-        
+  
     }else{
-        document.querySelector("#header-social").style.display = "inline";
         document.querySelector("#logoandtitle img").style.height = "8rem";
         document.querySelector("#logoandtitle h2").style.fontSize = "1.5rem";
+        document.querySelector("#logoandtitle h1").style.fontSize = "0.9rem";
+        document.querySelector("#header").style.height = "9rem";
+        document.querySelector("#header").style.padding = "2rem";
     }
 
 }
