983d3ab79b6bed1ed06af4840d710d557ab06f8c
Kevin del Hoyo <kevinalex11@hotmail.com>
2026-03-23 21:29:10 -0300

    glassEffect

--- js/inicio.js
+++ js/inicio.js
@@ -2,34 +2,37 @@
 var x = window.matchMedia("(min-width: 999px)")
 var y = window.matchMedia("(max-width: 999px)")
 
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
-    if (x.matches){
-        if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40){
+    if (x.matches) {
+        if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
             document.querySelector("#logoandtitle img").style.height = "4rem";
             document.querySelector("#logoandtitle h2").style.fontSize = "1rem";
             document.querySelector("#logoandtitle h1").style.fontSize = "0.5rem";
             document.querySelector("#header").style.height = "fit-content";
             document.querySelector("#header").style.padding = "0.5rem";
-    
-      
-        }else{
+            document.querySelector("#header").style.backgroundColor = "#f3f1f5c0";
+            document.querySelector("#header").style.backdropFilter = "blur(20px)";
+
+        } else {
             document.querySelector("#logoandtitle img").style.height = "8rem";
             document.querySelector("#logoandtitle h2").style.fontSize = "1.5rem";
             document.querySelector("#logoandtitle h1").style.fontSize = "0.9rem";
             document.querySelector("#header").style.height = "9rem";
             document.querySelector("#header").style.padding = "2rem";
+            document.querySelector("#header").style.backgroundColor = "transparent";
+            document.querySelector("#header").style.backdropFilter = "blur(0px)";
         }
     }
 }
 const parentElement = document.getElementById("header")
 
-function celphoneView(){
-    if (y.matches){
-            parentElement.style.position = "absolute";
-    }else{
-       parentElement.style.position = "fixed";
+function celphoneView() {
+    if (y.matches) {
+        parentElement.style.position = "absolute";
+    } else {
+        parentElement.style.position = "fixed";
     }
 }
 
@@ -40,14 +43,14 @@ celphoneView()
 const menuButton = document.querySelector("#menu-button");
 const menu = document.querySelector("#menu");
 
-function changeClass(){
+function changeClass() {
     let elementAtribute = menu.className;
     console.log('clicked')
-    if (elementAtribute == "hidden"){
+    if (elementAtribute == "hidden") {
         menu.className = "show";
         console.log(menu.className)
-    }else{
-        menu.className = "hidden"; 
+    } else {
+        menu.className = "hidden";
         console.log(menu.className)
     }
 }
